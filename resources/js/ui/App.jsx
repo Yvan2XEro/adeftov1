1bdd7676f7f02f9a26ecf87725c632636d644989
Yvan2Ero <yvankanaj@gmail.com>
2022-04-17 08:57:47 +0100

    Configuration du hot reload

--- resources/js/ui/App.jsx
+++ resources/js/ui/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import reactDom from 'react-dom';
+import { createTheme, ThemeProvider } from "@mui/material";
+import React, { useState } from "react";
+import reactDom from "react-dom/client";
 
 function App() {
-  return (
-    <div>App</div>
-  )
+    const [mode, setMode] = useState(localStorage.getItem("theme") || "light");
+    const onToggleDarkTheme = () => {
+        localStorage.setItem("theme", mode === "dark" ? "light" : "dark");
+        setMode(mode === "dark" ? "light" : "dark");
+    };
+
+    const theme = createTheme({ palette: { mode } });
+    return (
+        <ThemeProvider theme={theme}>
+            <div>uewuieuiefuie</div>
+        </ThemeProvider>
+    );
 }
 
-const root = reactDom.createRoot(document.getElementById('root'));
+const root = reactDom.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-

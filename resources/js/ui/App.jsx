9d73f778250bb90fdd2be5f315492c10e4c25ff4
Yvan2Ero <yvankanaj@gmail.com>
2022-04-17 09:06:21 +0100

    Configuration de react toastify

--- resources/js/ui/App.jsx
+++ resources/js/ui/App.jsx
@@ -1,6 +1,8 @@
 import { createTheme, ThemeProvider } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import reactDom from "react-dom/client";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     const [mode, setMode] = useState(localStorage.getItem("theme") || "light");
@@ -10,9 +12,13 @@ function App() {
     };
 
     const theme = createTheme({ palette: { mode } });
+    useEffect(() => {
+        toast.success("Welcome to React-Toastify!");
+        }, []);
     return (
         <ThemeProvider theme={theme}>
             <div>uewuieuiefuie</div>
+            <ToastContainer position="bottom-right" />
         </ThemeProvider>
     );
 }

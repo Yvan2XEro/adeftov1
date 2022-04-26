407ec15838b8fbe1d81de6d4ac04d4e637695c84
Yvan2Ero <yvankanaj@gmail.com>
2022-04-26 14:17:35 +0100

    Mise en place du loader dans les pages

--- resources/js/pages/admin/AdminUsersPage.jsx
+++ resources/js/pages/admin/AdminUsersPage.jsx
@@ -1,30 +1,30 @@
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  AppBar,
-  Badge,
-  Box,
-  Button,
-  FormControl,
-  Grid,
-  Icon,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-  Pagination,
-  Paper,
-  Switch,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TextField,
-  Typography,
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    AppBar,
+    Badge,
+    Box,
+    Button,
+    FormControl,
+    Grid,
+    Icon,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    ListSubheader,
+    Pagination,
+    Paper,
+    Switch,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    TextField,
+    Typography,
 } from "@mui/material";
 import React, { useCallback, useEffect, useState, useContext } from "react";
 import AddIcon from "@mui/icons-material/Add";
@@ -37,33 +37,32 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddMemberModal from "../../components/AddMemberModal";
 
 function AdminUsersPage() {
-  const [selected, setSetSelected] = useState(null);
-  const [openModal, setOpenModal] = React.useState(false);
-  return (
-    <Box mt={10} ml={2}>
-      <Box>
-        <Typography variant="h4">Gestion des Users</Typography>
-      </Box>
-      <Grid container columnSpacing={3}>
-        <Grid item xs={12} md={4}>
-          <Button
-          variant={selected ? "outlined" : "contained"}
+    const [selected, setSetSelected] = useState(null);
+    const [openModal, setOpenModal] = React.useState(false);
+    return (
+        <Box mt={10} ml={2}>
+            <Box>
+                <Typography variant="h4">Gestion des Users</Typography>
+            </Box>
+            <Grid container columnSpacing={3}>
+                <Grid item xs={12} md={4}>
+                    <Button
+                        variant={selected ? "outlined" : "contained"}
                         color="primary"
                         onClick={() => {
-                          setOpenModal(true);
-                      }}
+                            setOpenModal(true);
+                        }}
                         startIcon={<AddIcon />}
-            >         
-            Creer un nouvel utilisateur
-          </Button>
-          <AddMemberModal
-                open={openModal}
-                onClose={() => setOpenModal(false)}
-            />
-        </Grid>
-      </Grid>
-    </Box>
-
-  )
+                    >
+                        Creer un nouvel utilisateur
+                    </Button>
+                    <AddMemberModal
+                        open={openModal}
+                        onClose={() => setOpenModal(false)}
+                    />
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
 export default AdminUsersPage;

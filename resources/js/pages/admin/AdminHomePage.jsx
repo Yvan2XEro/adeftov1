30696203aa0b832bb008f85b861e33d432a6d56e
Armel Franck <franckzone2@gmail.com>
2022-04-27 15:28:32 +0100

    Chart Component With Recharts OK

--- resources/js/pages/admin/AdminHomePage.jsx
+++ resources/js/pages/admin/AdminHomePage.jsx
@@ -81,7 +81,7 @@ export default function AdminHomePage() {
                     </NavLink>
                 </Grid>
                 <Grid item xs={12} md={6} lg={4} sx={{ mt: 2, }}>
-                    <NavLink to="/admin/statitics" className="nav-link" activeclassname="active" Style="text-decoration: none">
+                    <NavLink to="/admin/statistics" className="nav-link" activeclassname="active" Style="text-decoration: none">
                         <Card component={Paper} sx={{mx: 1.5, my: 1}}>
                             <CardActionArea>
                                 <CardContent>
--- resources/js/pages/admin/AdminStatPage.jsx
+++ resources/js/pages/admin/AdminStatPage.jsx
@@ -24,14 +24,23 @@ import NotInterestedIcon from "@mui/icons-material/NotInterested";
 import EditUserModal from "../../components/EditUserModal";
 import { allUsers } from "../../services/usersServices";
 import Spinner from "../../components/Spinner";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import Chart from "../../components/Chart";
 
-const ITEMS_PER_PAGE = 5;
 function AdminStatPage() {
     return (
-        <>
-        </>
-    );
+        <Box mt={10} ml={2}>
+            <Box>
+                <Typography variant="h4">Statistiques</Typography>
+            </Box>
+            <Grid container md={12} xs={12}>
 
+                <Grid item md={6} xs={12} elevate={6}>
+                <Typography variant="h6" sx ={{mt:2}}>Evolution des utilisateurs </Typography>
+                    <Chart />
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
 export default AdminStatPage;

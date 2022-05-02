7b2de28dce8f356865a343e34a741be97627a00a
Yvan2Ero <yvankanaj@gmail.com>
2022-05-02 14:46:09 +0100

    Verification avant creation de la session

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,12 +21,31 @@ class Kernel extends ConsoleKernel
             $contributions = Contribution::where('is_active', true)->get();
             foreach ($contributions as $contribution) {
                 $members = $contribution->members;
-                // Save new Session in DB, with date from now + 30 days
+                // Save new Session in DB, with date from now + 30 days if not exists
+                $session = $contribution->sessions()->where('m', '==', date('m', strtotime('+30 days')))->first();
 
-                $contribution->sessions()->create([
-                    'date' => date('Y-m-d', strtotime('+30 days')),
-                ]);
-                // Notify all members
+                if (!$session) {
+                    $session = $contribution->sessions()->create([
+                        'date' => date('Y-m-d', strtotime('+30 days')),
+                        'month' => date('m', strtotime('+30 days')),
+                    ]);
+                    // Notify all members with swift mailer
+                    $transport = (new \Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
+                        ->setUsername(env('MAIL_USERNAME'))
+                        ->setPassword(env('MAIL_PASSWORD'));
+                    $mailer = new \Swift_Mailer($transport);
+
+                    foreach ($members as $member) {
+                        $message = (new \Swift_Message('Nouvelle session de contribution'))
+                        ->setFrom(env('MAIL_USERNAME'))
+                        ->setTo($member->pluck('email')->toArray())
+                        ->setBody(
+                            view('emails.session', ['contribution' => $contribution, 'session' => $session])->render(),
+                            'text/html'
+                        );
+                        $mailer->send($message);
+                    }
+                }
             }
         })->monthlyOn(+date('d'), date('H:i:s'));
     }
@@ -38,7 +57,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }

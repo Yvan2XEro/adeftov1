4813741284af60104fa3c0e4467b437821ad08c2
Yvan2Ero <yvankanaj@gmail.com>
2022-05-03 07:51:23 +0100

    Seeder Pour l'admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,14 +18,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $faker = \Faker\Factory::create();
+        // $faker = \Faker\Factory::create();
         $admin = User::create([
             'firstname' => "Admin",
             'lastname' => "Admin",
             'email' => "admin@adefto.com",
             'email_verified_at' => now(),
             'phone' => '0123456789',
-            'city' => $faker->randomElement($this->cities),
+            'city' => 'Dschang',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ]);
@@ -40,56 +40,56 @@ class UserSeeder extends Seeder
 
         $admin->save();
 
-        $j = User::create([
-            'firstname' => "John",
-            'lastname' => "Doe",
-            'email' => "john@gmail.com",
-            'email_verified_at' => now(),
-            'phone' => '0123456789',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-            'city' => $faker->randomElement($this->cities),
-        ]);
-        $j->attachRole('administrator');
-        $j->save();
+        // $j = User::create([
+        //     'firstname' => "John",
+        //     'lastname' => "Doe",
+        //     'email' => "john@gmail.com",
+        //     'email_verified_at' => now(),
+        //     'phone' => '0123456789',
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'remember_token' => Str::random(10),
+        //     'city' => $faker->randomElement($this->cities),
+        // ]);
+        // $j->attachRole('administrator');
+        // $j->save();
 
-        $contribution = Contribution::create([
-            'user_id' => $j->id,
-            'is_active' => true,
-            'name' => 'Adefto COTISATION',
-            'description' => $faker->text(100),
-        ]);
-        $contribution->members()->attach($j);
-        $contribution->save();
+        // $contribution = Contribution::create([
+        //     'user_id' => $j->id,
+        //     'is_active' => true,
+        //     'name' => 'Adefto COTISATION',
+        //     'description' => $faker->text(100),
+        // ]);
+        // $contribution->members()->attach($j);
+        // $contribution->save();
 
-        for ($i = 0; $i < 10; $i++) {
-            $user = User::create([
-                'firstname' => $faker->firstName,
-                'lastname' => $faker->lastName,
-                'email' => $faker->unique()->safeEmail,
-                'email_verified_at' => now(),
-                'phone' => '0123456789',
-                'city' => $faker->randomElement($this->cities),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-            ]);
-            $user->attachRole('user');
-            $user->save();
-            $contribution->membershipRequests()->create([
-                'user_id' => $user->id,
-                'is_accepted' => true,
-            ]);
-            $contribution->members()->attach($user);
-            $session = $contribution->sessions()->create([
-                'date' => '2022-' . (($i + 1) < 10 ? '0' . $i + 1 : $i) . '-15 23:14:21',
-            ]);
-            $session->payments()->create([
-                'amount' => $faker->numberBetween(100, 1000),
-                'status' => 'paid',
-                'user_id' => $user->id,
-                'session_id' => $session->id,
-            ]);
-        }
+        // for ($i = 0; $i < 10; $i++) {
+        //     $user = User::create([
+        //         'firstname' => $faker->firstName,
+        //         'lastname' => $faker->lastName,
+        //         'email' => $faker->unique()->safeEmail,
+        //         'email_verified_at' => now(),
+        //         'phone' => '0123456789',
+        //         'city' => $faker->randomElement($this->cities),
+        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //         'remember_token' => Str::random(10),
+        //     ]);
+        //     $user->attachRole('user');
+        //     $user->save();
+        //     $contribution->membershipRequests()->create([
+        //         'user_id' => $user->id,
+        //         'is_accepted' => true,
+        //     ]);
+        //     $contribution->members()->attach($user);
+        //     $session = $contribution->sessions()->create([
+        //         'date' => '2022-' . (($i + 1) < 10 ? '0' . $i + 1 : $i) . '-15 23:14:21',
+        //     ]);
+        //     $session->payments()->create([
+        //         'amount' => $faker->numberBetween(100, 1000),
+        //         'status' => 'paid',
+        //         'user_id' => $user->id,
+        //         'session_id' => $session->id,
+        //     ]);
+        // }
     }
 
     private

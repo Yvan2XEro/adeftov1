6816b0b8ade3cc41428e78a7c702724ffdbf7bdc
Yvan2XEro <yvankanaj@gmail.com>
2022-11-19 18:23:45 +0100

    Mise en place des images et logo

--- resources/js/components/ImageTheme.jsx
+++ resources/js/components/ImageTheme.jsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Grid } from "@mui/material";
-import a1 from "../assets/images/asset1.jpg"
-import a2 from "../assets/images/asset2.jpg"
 import a3 from "../assets/images/asset3.jpg"
 import a4 from "../assets/images/asset4.jpg"
 import a6 from "../assets/images/home/asset1.png"
@@ -13,7 +11,7 @@ import a10 from "../assets/images/home/asset5.png"
 import a11 from "../assets/images/home/asset6.png"
 import a12 from "../assets/images/home/asset7.png"
 
-const images = [a1, a2, a3, a4, a6, a7, a8, a9, a10, a11, a12];
+const images = [a3, a4, a6, a7, a8, a9, a10, a11, a12];
 function ImageTheme() {
     const [image, setImage] = React.useState(images[0]);
     React.useEffect(() => {
